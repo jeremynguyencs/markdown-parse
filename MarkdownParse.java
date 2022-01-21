f01cc744bf1d3e45c65d34420b952fbe2afc7cd8
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2022-01-20 19:25:43 -0800

    Update MarkdownParse.java with determineLink and returnLink method

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,29 +1,58 @@
+
 // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+// Split string from https://www.geeksforgeeks.org/split-string-java-examples/
+// Indexof from https://www.w3schools.com/java/ref_string_indexof.asp
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParse {
-    public static ArrayList<String> getLinks(String markdown) {
-        ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then take up to
-        // the next )
-        int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
-        }
-        return toReturn;
+  public static ArrayList<String> getLinks(String markdown) {
+    ArrayList<String> toReturn = new ArrayList<>();
+    String[] lines = markdown.split("\n");
+    for (String line : lines) {
+      if (determineLink(line)) {
+        toReturn.add(returnLink(line));
+      }
     }
-    public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
-        ArrayList<String> links = getLinks(contents);
-        System.out.println(links);
+    return toReturn;
+  }
+
+  public static boolean determineLink(String line) {
+    // markdown link requires "[", "]", "(" and ")"
+    if (line.contains("[") && line.contains("]") && line.contains("(") && line.contains(")")) {
+      // check if line contains a ] that follows a [ or a ) that follows a (
+      int indexOfBracket = line.indexOf("]", line.indexOf("["));
+      int indexOfParen = line.indexOf(")", line.indexOf("("));
+      if (indexOfBracket == -1 || indexOfParen == -1) {
+        // if there is no ] after the [ or no ) after the (, then it is not a link
+        return false;
+      }
+      // if the the letter directly after ] is not (, then it is not a link
+      if (line.charAt(indexOfBracket + 1) != '(') {
+        return false;
+      }
+      return true;
     }
+    return false;
+  }
+
+  public static String returnLink(String line) {
+    String link = "";
+    // get the text between the ( and the )
+    link = line.substring(line.indexOf("(") + 1, line.indexOf(")"));
+    // throw error if link is not formatted correctly
+    if (!determineLink(line) || link.equals("")) {
+      throw new IllegalArgumentException("Link is not formatted correctly");
+    }
+    return link;
+  }
+
+  public static void main(String[] args) throws IOException {
+    Path fileName = Path.of(args[0]);
+    String contents = Files.readString(fileName);
+    ArrayList<String> links = getLinks(contents);
+    System.out.println(links);
+  }
 }
--- new-file.md
+++ new-file.md
@@ -1,8 +1,15 @@
-[https://penisland.net/](eCommerce site for pens (and some other things))
+[eCommerce site for pens and some other things](https://penisland.net/)
 [brackets by themselves]
-<https://www.anotherkindoflink.com>
 [a link!](https://something.com)
 [another link!](some-page.html)
+[jeremy
+[jeremy]
+(jeremy)
+[jeremy](
+[jeremy])
 [https://jeremy.com] some other words 
 hey im here
 (im kinda far away)
+)
+]
+[]
